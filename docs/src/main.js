5a8e9a558b4809fe3394bd67f0e3dd3c3d4a4ffa
Filip Vrba <filipvrba@pm.me>
2022-12-18 18:55:02 +0100

    nevim

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,28 +1,28 @@
-const engineList = document.getElementById('eng-list')
-const location = window.location.href.slice(0, -1)
-const p_engine = "public/engine"
+const engineList = document.getElementById('eng-list');
+const location = window.location.href.slice(0, -1);
+const p_engine = "public/engine";
 
 async function getEngineLog(callback) {
-    const url = `${location}/${p_engine}/log.txt`
+    const url = `${location}/${p_engine}/log.txt`;
     let response = await fetch( url );
 
     if ( response.status === 200 ) {
         const content = await response.text();
-        callback(content.trim().split('\n'))
+        callback(content.trim().split('\n'));
     } else if ( response.status === 404 ) {
-        callback(null)
+        callback(null);
     }
 }
 
 function h_addLi(arr, exten = "js") {
     if (arr === null) return;
 
-    let list = ""
+    let list = "";
     arr.forEach((e) => {
-        const url = `${location}/${p_engine}/${exten}/${e}.${exten}`
-        list += `<li><a href="${url}">${e}</a></li>\n`
+        const url = `${location}/${p_engine}/${exten}/${e}.${exten}`;
+        list += `<li><a href="${url}">${e}</a></li>\n`;
     })
-    engineList.innerHTML = list.trim()
+    engineList.innerHTML = list.trim();
 }
 
-getEngineLog(h_addLi)
+getEngineLog(h_addLi);
